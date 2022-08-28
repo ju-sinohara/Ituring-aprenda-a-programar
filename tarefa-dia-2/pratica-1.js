15fa3efa8f63cb384883e088ede3bb0d3e766e01
Juliany Sinohara <juliany.sinohara@gmail.com>
2022-08-27 23:46:38 -0300

    terceira alteracao

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,11 @@
+let nome = "Juliany"
+let email = "juliany.sinohara@gmail.com"
+let profissao = "adm"
+let idade = 35
+let lugardossonhos = "maldivas"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugardossonhos)
