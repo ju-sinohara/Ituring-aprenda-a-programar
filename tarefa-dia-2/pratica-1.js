035e62e657b41c25ad24cbc44f4801b3c3a117c4
Juliany Sinohara <juliany.sinohara@gmail.com>
2022-08-28 18:49:02 -0300

    4a atualizacao

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,3 +9,5 @@ console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(lugardossonhos)
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugardossonhos} . Confirma pra gente alguns dados? A sua idade é ${idade}, voce é ${profissao} e o seu email ${email}? Ficamos no aguardo. Muito Obrigado e boa viagem! :D`)
