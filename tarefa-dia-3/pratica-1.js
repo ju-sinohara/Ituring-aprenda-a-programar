6da333b693e13c76bad5ae918a65d99a706f4326
Juliany Sinohara <juliany.sinohara@gmail.com>
2022-08-30 19:48:49 -0300

    5a atualizacao

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,12 @@
+//exibir todos os numeros impares 
+
+let num1 = 1
+let num2 = 10
+let cont = 1
+
+while (num1 <= num2)
+{
+    if(num1 % 2 == 1)
+        console.log(num1)
+    num1++
+}
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,8 @@
+let numero = 16
+i = 1
+
+while  (i <= numero) {
+    if (i %2 !=0)
+    console.log (i)
+i++
+}
